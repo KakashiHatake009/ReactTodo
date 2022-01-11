46264fd86b68b683f72bd48f5a778c35f1498da7
Prashanth K <prashanth.kuntlapalli@gmail.com>
2022-01-11 08:28:17 +0100

    toDo list

---
+++ src/components/Todo.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Backdrop from "./Backdrop";
+import Modal from "./Modal";
+
+function Todo(props) {
+  const [isModalOpen, setModalState] = useState(false);
+  console.log(isModalOpen);
+
+  const deleteHandler = function () {
+    setModalState(true);
+  };
+
+  const closeModalHandler = function () {
+    setModalState(false);
+  };
+  console.log(props);
+  return (
+    <div>
+      <div className="card">
+        <h2>{props.title}</h2>
+        <div className="actions">
+          <button className="btn" onClick={deleteHandler}>
+            Delete
+          </button>
+        </div>
+      </div>
+      {isModalOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {isModalOpen && <Backdrop onCancel={closeModalHandler} />}
+    </div>
+  );
+}
+
+export default Todo;
